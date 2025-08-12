b1c9102281f9300871ecc70913c165a848f8906c
male-gal <maxence.le-gall@docaposte.fr>
2025-08-12 18:34:11 +0200

    Initial Commit

---
+++ MacGestures/MacGestures.h
@@ -0,0 +1,12 @@
+//
+//  MacGestures.h
+//  MacGestures
+//
+//  Created by LE GALL Maxence on 12/08/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MacGestures : NSObject
+
+@end
---
+++ MacGestures/MacGestures.m
@@ -0,0 +1,12 @@
+//
+//  MacGestures.m
+//  MacGestures
+//
+//  Created by LE GALL Maxence on 12/08/2025.
+//
+
+#import "MacGestures.h"
+
+@implementation MacGestures
+
+@end
